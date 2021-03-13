c6743d472abffeb0459b769ef0a62c83956e01ef
StanSimona-Daniela <stansdaniela@gmail.com>
2021-03-13 12:24:47 +0200

    seminar 3 continuare- DRY

--- seminar2/src/ro/ase/cts/clase/Elev.java
+++ seminar2/src/ro/ase/cts/clase/Elev.java
@@ -10,22 +10,24 @@ public class Elev extends Aplicant{
 	public int getClasa() {
 		return clasa;
 	}
+
 	public void setClasa(int i) {
 		this.clasa = i;
 	}
+
 	public String getTutore() {
 		return tutore;
 	}
+
 	public void setTutore(String tutore) {
 		this.tutore = tutore;
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "Elev: Nume=" + nume + ", Prenume=" + prenume + ", Varsta="
 				+ varsta + ", Punctaj=" + punctaj + ", Nr_proiecte=" + nr_proiecte  + ", DenumireProiect="
-				+ Arrays.toString(denumireProiect) + "Clasa=" + clasa + ", Tutore=" + tutore;
+				+ Arrays.toString(denumiriProiecte) + "Clasa=" + clasa + ", Tutore=" + tutore;
 	}
 	
 	public Elev() {
--- seminar2/src/ro/ase/cts/program/Program.java
+++ seminar2/src/ro/ase/cts/program/Program.java
@@ -1,7 +1,5 @@
 package ro.ase.cts.program;
-
-
-import ro.ase.cts.clase.readers.*;
+import ro.ase.cts.clase.readers.*;    //pt return
 import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.clase.readers.Readable;
@@ -9,23 +7,21 @@ import ro.ase.cts.clase.readers.Readable;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-
 public class Program {
 
-	public static List<Aplicant> citesteAplicanti (String numeFisier, Readable reader) throws FileNotFoundException {
-		return reader.readAplicanti(numeFisier);
+	public static List<Aplicant> citesteAplicanti(Readable reader) throws FileNotFoundException {
+		return reader.readAplicanti();
 	}
 
 	public static void main(String[] args) {
-		List<Aplicant> listaAplicanti;
+		List<Aplicant> listaAplicant;
 		try {
-			listaAplicanti = citesteAplicanti("seminar2/elevi.txt", new EleviReader());
-			for(Aplicant aplicant:listaAplicanti)
+			listaAplicant = citesteAplicanti(new EleviReader("Seminar2/elevi.txt"));
+			for(Aplicant aplicant:listaAplicant)
 				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 }
